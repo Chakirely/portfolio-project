e8cfa9dd85cd0ad1e613527e12ae9364bf4ba5d6
chakir <chakirely@gmail.com>
2019-02-08 21:53:25 +0000

    requirements

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'portfoliodb',
-        'USER':'postgres',
-        'PASSWORD': 'Marouaninu!2',
+        'USER':'chakir',
+        'PASSWORD': 'suPerUser1_',
         'HOST':'localhost',
         'PORT' : '5432',
 
